eb3d1e8c1aea7ea5a6b45d85714cefc79be102b2
JQB <jordanqbehn@gmail.com>
2021-01-09 12:09:23 -0600

    initial commit of database ainitialization and seed values

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Poppy', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Oscar', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Smudge', true);
