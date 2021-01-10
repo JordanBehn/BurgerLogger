f4c9672f46094d4b2cbfd3853b49fe47eec717ec
JQB <jordanqbehn@gmail.com>
2021-01-09 21:02:19 -0600

    commit of burger controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var burger = require("../models/burger.js");
+
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne(
+        ["burger_name"], [
+            req.body.burger_name
+        ],
+        function() {
+            // Send back the ID of the new quote
+            //res.json({ id: result.insertId });
+        });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function() {});
+});
+
+// Export routes for server.js to use.
+module.exports = router;
